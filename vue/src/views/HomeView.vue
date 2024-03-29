c0a584250cd76160c629a6ff72e01e46a42deaa3
nanchengcyu <1146137024@qq.com>
2024-03-30 02:08:33 +0800

    vue页面开发

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,57 +1,77 @@
 <template>
 
-  <div>
+
+  <el-container>
+
+    <el-aside :width="asideWidth" style="min-height: 100vh;background-color:#001529 ">
+      <div
+          style="height: 60px;color: white;display: flex;align-items: center;justify-content: center">
+        <img src="@/assets/logo.png" alt="logo" style="width: 40px;height: 40px">
+        <span class="logo-title" v-show="!isCollapse">Errand</span>
+      </div>
+
+      <el-menu :collapse="isCollapse"
+               :collapse-transition="false"
+               default-active="2"
+               class="el-menu-vertical-demo"
+               background-color="#001529"
+               active-text-color="#fff"
+               text-color="rgba(255,255,255,0.65)"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航一</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-submenu>
+          <template slot="title">
+            <i class="el-icon-document"></i>
+            <span slot="title">信息管理</span>
+          </template>
+          <el-menu-item>用户信息</el-menu-item>
+        </el-submenu>
+      </el-menu>
+
+    </el-aside>
+
+
     <el-container>
 
-      <el-aside :width="asideWidth" style="min-height: 100vh;background-color:#001529 ">
-        <div
-            style="height: 60px;color: white;display: flex;align-items: center;justify-content: center">
-          logo
+      <el-header>
+        <i :class="collapseIcon" style="font-size: 26px" @click="handleCollapse"></i>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+        <div style="flex: 1;width: 0;display: flex;align-items: center;justify-content: flex-end">
+        <i class="el-icon-full-screen" style="font-size: 26px" @click="handleFull"></i>
+          <el-dropdown>
+          <div style="display: flex;align-items: center " cursor="default">
+            <img src="@/assets/logo.png" alt="logo" style="width: 40px;height: 40px;margin: 0 5px">
+            <span>管理员</span>
+          </div>
+          <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人信息</el-dropdown-item>
+          <el-dropdown-item>修改密码</el-dropdown-item>
+          <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
         </div>
-
-        <el-menu :collapse="isCollapse"
-                 :collapse-transition="false"
-                 default-active="2"
-                 class="el-menu-vertical-demo"
-                 background-color="#001529"
-                 active-text-color="#fff"
-                 text-color="rgba(255,255,255,0.65)"
-        >
-          <el-menu-item index="1">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航一</span>
-          </el-menu-item>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-submenu>
-            <template slot="title">
-              <i class="el-icon-document"></i>
-              <span slot="title">信息管理</span>
-            </template>
-            <el-menu-item>用户信息</el-menu-item>
-          </el-submenu>
-        </el-menu>
-
-      </el-aside>
-
-
-      <el-container>
-
-        <el-header>
-          <i class="el-icon-menu" style="width: 40px;margin-top: 20px" @click="handleCollapse"></i>
-          Header
-        </el-header>
-        <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
     </el-container>
-  </div>
+  </el-container>
+
 </template>
 
 
@@ -61,13 +81,18 @@ export default {
   data() {
     return {
       isCollapse: false,
-      asideWidth: '200px'
+      asideWidth: '200px',
+      collapseIcon:'el-icon-s-fold'
     }
   },
   methods: {
+    handleFull(){
+      document.documentElement.requestFullscreen()
+    },
     handleCollapse() {
       this.isCollapse = !this.isCollapse
-      this.asideWidth = this.isCollapse?'64px':'200px'
+      this.asideWidth = this.isCollapse ? '64px' : '200px'
+      this.collapseIcon = this.isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'
     }
   }
 
@@ -88,5 +113,29 @@ export default {
   background-color: #40a9ff !important;
   border-radius: 5px !important;
   margin: 4px !important;
+}
+
+.el-menu-item.is-active i, .el-menu-item.is-active .el-tooltip {
+  margin-left: -4px;
+}
+
+.el-menu-inline .el-menu-item.is-active {
+  padding-left: 45px !important;
+}
+
+.el-side {
+  transition: width .3s;
+  box-shadow: 2px 0 6px rgba(0,21,41,35);
+}
+
+.logo-title {
+  margin-left: 5px;
+  font-size: 20px;
+  transition: all .3s;
+}
+.el-header{
+  box-shadow: 2px 0 6px rgba(0,21,41,35);
+  display: flex;
+  align-items: center;
 }
 </style>
